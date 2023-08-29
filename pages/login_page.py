565a85424311a790f16287c3d799b70764690a09
legolaisik <legolas-lego@mail.ru>
2023-08-29 08:10:36 +0300

    add class to combine tests guest login

--- pages/login_page.py
+++ pages/login_page.py
@@ -20,3 +20,9 @@ class LoginPage(BasePage):
     def should_be_register_form(self):
         assert self.is_element_present(*LoginPageLocators.REGISTER_FORM), \
             "Register form is not presented"
+
+    def register_new_user(self, email, password):
+       self.browser.find_element(*LoginPageLocators.REGISTER_EMAIl).send_keys(email)
+       self.browser.find_element(*LoginPageLocators.REGISTER_PASSWORD1).send_keys(password)
+       self.browser.find_element(*LoginPageLocators.REGISTER_PASSWORD2).send_keys(password)
+       self.browser.find_element(*LoginPageLocators.SUBMIT_REGISTER).click()
